e2216448d27cc564c5a64812c17338abe8cfa938
Colin Pieper <4346246+colinpieper@users.noreply.github.com>
2022-10-19 21:31:32 +0200

    Update configuration format

--- util/config.go
+++ util/config.go
@@ -3,8 +3,24 @@ package util
 import "github.com/spf13/viper"
 
 type Config struct {
-	RabbitMQURI string `mapstructure:"RABBITMQ_URI"`
-	DatabaseURI string `mapstructure:"DATABASE_URI"`
+	RabbitMQ rabbitMQConfig `mapstructure:",squash"`
+	Postgres postgresConfig `mapstructure:",squash"`
+}
+
+type rabbitMQConfig struct {
+	Username string `mapstructure:"RABBITMQ_USER"`
+	Password string `mapstructure:"RABBITMQ_PASS"`
+	Host     string `mapstructure:"RABBITMQ_HOST"`
+	Port     string `mapstructure:"RABBITMQ_PORT"`
+	Vhost    string `mapstructure:"RABBITMQ_VHOST"`
+}
+
+type postgresConfig struct {
+	Username string `mapstructure:"POSTGRES_USER"`
+	Password string `mapstructure:"POSTGRES_PASSWORD"`
+	Host     string `mapstructure:"POSTGRES_HOST"`
+	Port     string `mapstructure:"POSTGRES_PORT"`
+	Database string `mapstructure:"POSTGRES_DB"`
 }
 
 func LoadConfig() (Config, error) {
